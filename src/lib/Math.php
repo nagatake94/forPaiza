0aa9ed27da6ce90e93ff2f392f8fa069cec63a41
永田圭 <nagatakei@nagatakeinoMacBook-Air.local>
2021-06-20 21:29:29 +0900

    doneChoiceMinNum

--- src/lib/Math.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-function double($num)
-{
-    return $num * 2;
-}
---
+++ src/tests/AddNumTest.php
@@ -0,0 +1,12 @@
+<?php
+
+// use PHPUnit\Framework\TestCase;
+
+// class AddNumTest extends TestCase
+// {
+//     public function testAddNum()
+//     {
+//         require_once(__DIR__ . '/../lib/AddNum.php');
+//         $this->assertSame(5, addNum("2 3"));
+//     }
+// }
